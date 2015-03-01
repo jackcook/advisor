43d294738da234ae6804b5398c9f4fa0177226a3
Jack Cook <jack@jackcook.nyc>
2015-03-01 08:16:05 -0600

    Re-added some templates

--- node/routes/index.js
+++ node/routes/index.js
@@ -6,6 +6,10 @@ var lang = require('../lang');
 var Diffbot = require('diffbot').Diffbot;
 var diffbot = new Diffbot('0d5c56d2a7a3a5a4ad6c644b326993c2');
 
+exports.index = function(req, res) {
+  res.render('index', {hideHeader: true});
+}
+
 router.get('/:service', function(req, res, next) {
   var service = service_to_url(req.params.service);
   diffbot.article({uri: service}, function(err, resp, req) {
