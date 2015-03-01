940f794cc53de4cf5e93453c156ff8529bf91954
Jack Cook <jack@jackcook.nyc>
2015-02-28 23:04:36 -0600

    Implemented diffbot in mode, removed main.py

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var Diffbot = require('diffbot').Diffbot;
+var diffbot = new Diffbot('0d5c56d2a7a3a5a4ad6c644b326993c2');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  diffbot.article({uri: 'https://www.spotify.com/us/legal/end-user-agreement/'}, function(err, resp) {
+    res.send(resp.text);
+  });
 });
 
 module.exports = router;
