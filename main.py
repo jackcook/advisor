2ab50d236ca8bff4d69f68e275875a451f10a93a
Jack Cook <jack@jackcook.nyc>
2015-02-28 20:07:31 -0600

    Fetch ToS text from websites

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from bottle import route, run, request
-import http.client
+import json
+import requests
+from pprint import pprint
 
 @route('/')
 def main():
-  return "hello world"
+    r = requests.get(
+        url="http://api.diffbot.com/v3/article",
+        params = {
+            "token": "0d5c56d2a7a3a5a4ad6c644b326993c2",
+            "url": "https://www.spotify.com/us/legal/end-user-agreement/"
+        }
+    )
+
+    data = r.json()
+    tos = data["objects"][0]["text"]
+
+    return tos
 
 run(host='localhost', port=8080, debug=True)
